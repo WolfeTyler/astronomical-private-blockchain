ae02b9aa4fd19bfd6c1919e8cfea0bad259f1d53
Tyler Wolfe <tbwolfe@live.com>
2021-04-30 09:22:45 -0400

    Added validateChain endpoint

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -116,6 +117,18 @@ class BlockchainController {
             }
             
         });
+    }
+    
+    // This endpoint allows you to validate the blockchain
+    validateChain() { 
+        this.app.get("/validateChain", async (req, res) => { 
+            let errorList = await this.blockchain.validateChain(); 
+            if(errorList.length > 0) 
+                { 
+                    return res.status(500).json(errorList);
+                } else { return res.status(202).send("Blockchain validated"); } 
+            
+        }); 
     }
 
 }
